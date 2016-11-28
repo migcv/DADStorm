e1159efde57da35995ac6d0fa5e78b1b49569509
afpereira13 <alexandre.figueiredo.pereira@tecnico.ulisboa.pt>
2016-11-28 15:15:56 +0000

    #11 now call method

--- CommonTypes/operators/Custom.cs
+++ CommonTypes/operators/Custom.cs
@@ -6,30 +6,31 @@ using System.Reflection;
 namespace CommonTypes.operators {
     public class Custom : RemoteOperator {
         private string dll, className, method;
-        private Type t;
-        private MethodInfo m;
 
         public Custom(string[] inputSources, string[] outputSources, string routing, bool logLevel, string[] parameters)
             : base("CUSTOM", inputSources, outputSources, routing, logLevel, parameters){
         }
 
         public override void doOperation(Tuple input) {
-            try{
-                if (parameters.Length == 3)                {
-                    this.dll = "../../../input/" + parameters[0];
-                    this.className = parameters[1];
-                    this.method = parameters[2];
-                }
+           if (parameters.Length == 3)                {
+                this.dll = "C:/Users/Admin/Documents/GitHub/DADStorm/input/" + parameters[0];
+                this.className = parameters[1];
+                this.method = parameters[2];
+            }
 
-                byte[] code = File.ReadAllBytes(dll);
-                Assembly assembly = Assembly.Load(code);
-
-                foreach (Type type in assembly.GetTypes()){
-                    if (type.IsClass == true){
-                        if (type.FullName.EndsWith("." + className)){
+            byte[] code = File.ReadAllBytes(dll);
+            Assembly assembly = Assembly.Load(code);
+            try
+            {
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (type.IsClass == true)
+                    {
+                        if (type.FullName.EndsWith("." + className))
+                        {
                             // create an instance of the object
                             object ClassObj = Activator.CreateInstance(type);
-                            object resultObject = type.InvokeMember("CustomOperation",
+                            object resultObject = type.InvokeMember(method,
                                 BindingFlags.Default | BindingFlags.InvokeMethod, null, ClassObj,
                                 new Object[] { input });
                             result = (Tuple)resultObject;
@@ -37,9 +38,16 @@ namespace CommonTypes.operators {
                     }
                 }
             }
-            catch {
-				result = input;
-			}
+            catch
+            (ReflectionTypeLoadException ex){
+                // now look at ex.LoaderExceptions - this is an Exception[], so:
+                foreach (Exception inner in ex.LoaderExceptions){
+                    // write details of "inner", in particular inner.Message
+                    Console.WriteLine(inner.Message);
+                    result = input;
+                }
+
+            }
         }
     }
 }
